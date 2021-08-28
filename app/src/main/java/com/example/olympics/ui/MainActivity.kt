538e12da533b85bed299e4d89b016ef9acf0fba2
Wesam Nabeel <wesamezio@gmail.com>
2021-08-28 23:45:26 +0300

    open the assets file

--- app/src/main/java/com/example/olympics/ui/MainActivity.kt
+++ app/src/main/java/com/example/olympics/ui/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.olympics.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.olympics.R
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Olympics)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        parseTheData()
+    }
+
+    private fun parseTheData() {
+       val inputStream = assets.open("tokyo_2021.csv")
+       val buffer = BufferedReader(InputStreamReader(inputStream))
+       buffer.forEachLine {
+           Log.v ("MAIN_ACTIVITY",it)
+       }
     }
 }
